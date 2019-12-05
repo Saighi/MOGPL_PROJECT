4b647e5620787ecbfaedf553b0f99b89b2f494b9
paulsaighi <paulsaighi@hotmail.com>
2019-12-06 00:52:20 +0100

    facultative en cours

--- facultative.py
+++ facultative.py
@@ -1,21 +1,68 @@
 from probability import *
+from gurobi_optimisation import *
 
-def eg_d(i,j,table_d1,table_d2,Eg_table_j1_k_l):
-    sum_1=0
+
+def eg_d(i, j, table_d1, table_d2, Eg_table_j1_k_l):
+    sum_1 = 0
     for k in range(len(table_d1)):
-        sum_2=0
+        sum_2 = 0
         for l in range(len(table_d2)):
-            sum_2 += table_d2[j]* Eg_table_j1_k_l[i+k,j+l]
-        sum_1+= table_d1[i] * sum_2
+            sum_2 += table_d2[l] * Eg_table_j1_k_l[i + k, j + l]
+        sum_1 += table_d1[k] * sum_2
     return sum_1
 
-def eg_d_table(i,j,table_D,Eg_table_j1_k_l):
-    D = len(table_D)
-    table_eg_d = np.zeros((D,D))
 
+def eg_d_table(i, j, table_D, Eg_table_j1_k_l):
+    D = len(table_D)-1
+    table_eg_d = np.zeros((D, D))
     for x in range(D):
         for y in range(D):
-            table_eg_d[x,y] = eg_d(i, j, table_D[x+1], table_D[y+1], Eg_table_j1_k_l)
-            pass
+            table_eg_d[x, y] = eg_d(i, j, table_D[x + 1], table_D[y + 1], Eg_table_j1_k_l)
 
     return table_eg_d
+
+
+def eg_simu_meta(i, j, D, N):
+    table_D = p_table(D)
+    Eg_table_j1 = np.full((N, N), 1000.)
+
+    # Eg_table_which_play_j1 = np.full((N, N), 1000.)
+    def eg_simu(i, j):
+
+        if i == j and i!= 0:
+            return 0
+
+        if i >= N and j >= N:
+            return 0
+        if i >= N:
+            return 1
+        if j >= N:
+            return -1
+
+        Eg_table_j1_k_l = np.zeros((N+(6*D), N+(6*D)))
+
+        for x in range(i+1, D+(6*D)):
+            for y in range(j+1, D+(6*D)):
+                if x < N and y < N and Eg_table_j1[x, y] != 1000.:
+                    Eg_table_j1_k_l[x, y] = Eg_table_j1[x, y]
+                else:
+                    Eg_table_j1_k_l[x, y] = eg_simu(x, y)
+
+        eg_d_t = eg_d_table(i, j, table_D, Eg_table_j1_k_l)
+        print(eg_d_t)
+        strat_opti = strat_opt_simu_mat(eg_d_t)
+
+        eg = 0
+        for x in range(len(strat_opti)):
+            sum = 0
+            for y in range(len(strat_opti)):
+                sum += strat_opti[y] * eg_d_t[x, y]
+            eg += strat_opti[x] * sum
+        if Eg_table_j1[i, j] == 1000.:
+            Eg_table_j1[i, j] = eg
+        return eg
+
+    return eg_simu(i, j), Eg_table_j1
+
+
+eg_fac = eg_simu_meta(0, 0, 2, 10)
--- gurobi_optimisation.py
+++ gurobi_optimisation.py
@@ -1,52 +1,50 @@
 #!/usr/bin/python
 
+from gurobipy import *
+
 # Copyright 2013, Gurobi Optimization, Inc.
 from simultane import *
 
-from gurobipy import *
 
 def strat_opt_simu(D):
-
     matrice_de_gain = EGmatP(D)
-
-    nbcont = D+1
+    nbcont = D + 1
     nbvar = D
 
     # Range of plants and warehouses
-    lignes = range(nbcont-1)
+    lignes = range(nbcont - 1)
     colonnes = range(nbvar)
 
     # Matrice des contraintes
     a = matrice_de_gain
 
-
     # Coefficients de la fonction objectif
-    c = [0]*D
 
     m = Model("mogplex")
+    m.setParam('OutputFlag', False)
 
     # declaration variables de decision
     x = []
     for i in colonnes:
         x.append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i + 1)))
 
+    val_max = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="val_min")
+
     # maj du modele pour integrer les nouvelles variables
     m.update()
 
-    obj = LinExpr();
-    obj = 0
-    for j in colonnes:
-        obj += c[j] * x[j]
+    obj = val_max
 
     # definition de l'objectif
     m.setObjective(obj, GRB.MAXIMIZE)
 
     # Definition des contraintes
-    for i in lignes:
-        m.addConstr(quicksum(a[i][j] * x[j] for j in colonnes) <= 0, "Contrainte%d" % i)
-    left_member=0
+    for i in colonnes:
+        m.addConstr(val_max - quicksum(a[j][i] * x[j] for j in lignes) <= 0, "Contrainte%d" % i)
+
+    left_member = 0
     for i in range(D):
-        left_member+=x[i]
+        left_member += x[i]
 
     m.addConstr(left_member == 1, "Contrainte%d" % 4)
 
@@ -62,9 +60,68 @@ def strat_opt_simu(D):
 
     strat_vec = np.zeros(D)
     for j in colonnes:
-        strat_vec[j]=x[j].x
-
+        strat_vec[j] = x[j].x
     return strat_vec
 
 
-strat_vec=strat_opt_simu(10)
+def strat_opt_simu_mat(matrice_de_gain):
+    D = len(matrice_de_gain)
+
+    nbcont = D + 1
+    nbvar = D
+
+    # Range of plants and warehouses
+    lignes = range(nbcont - 1)
+    colonnes = range(nbvar)
+
+    # Matrice des contraintes
+    a = matrice_de_gain
+
+    # Coefficients de la fonction objectif
+
+    m = Model("mogplex")
+    m.setParam('OutputFlag', False)
+
+    # declaration variables de decision
+    x = []
+    for i in colonnes:
+        x.append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i + 1)))
+
+    val_max = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="val_min")
+
+    # maj du modele pour integrer les nouvelles variables
+    m.update()
+
+    obj = val_max
+
+    # definition de l'objectif
+    m.setObjective(obj, GRB.MAXIMIZE)
+
+    # Definition des contraintes
+    for i in colonnes:
+        m.addConstr(val_max - quicksum(a[j][i] * x[j] for j in lignes) <= 0, "Contrainte%d" % i)
+
+    left_member = 0
+    for i in range(D):
+        left_member += x[i]
+
+    m.addConstr(left_member == 1, "Contrainte%d" % 4)
+
+    # Resolution
+    m.optimize()
+
+    """print("")
+    print('Solution optimale:')
+    for j in colonnes:
+        print('x%d' % (j + 1), '=', x[j].x)
+    print("")
+    print('Valeur de la fonction objectif :', m.objVal)"""
+
+    strat_vec = np.zeros(D)
+    for j in colonnes:
+        strat_vec[j] = x[j].x
+
+    return strat_vec
+
+# opt = EGmatP(2)
+# strat_vec=strat_opt_simu(10)
